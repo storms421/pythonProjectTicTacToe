8ffdd5c18ce22fe417ad35e9e5aab3b0b799c0bd
Noah Storms <noahstorms@yahoo.com>
2024-01-05 21:22:16 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def multiplayer():
+
+    game_win = False
+
+    while(game_win == False):
+        game_win = True
+
+
+def display_table():
+    for i in range(len(tic_tac_toe_board)):
+        for j in range(len(tic_tac_toe_board[i])):
+            print(tic_tac_toe_board[i][j], end="")
+            if j < 2:
+                print("|", end="")
+        print("")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Welcome to Tic-Tac-Toe!")
+
+    tic_tac_toe_board = [["  ", " X ", " O "], [" O ", "  ", "  "], [" X ", "  ", " X "]]
+
+    display_table()
+   # player1_input = input("Player 1! Enter coordinates to place your piece down: ")
+
+   # multiplayer()
+
+
